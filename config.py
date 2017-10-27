d84e13961c7bbca223698a48c9bb8d96ad637604
yashshanker <yash.shanker@outlook.com>
2017-10-27 10:40:18 +0530

    added twitter-analyzer

---
+++ config.py
@@ -0,0 +1,11 @@
+
+twitter_consumer_key = '5p3eDOHKn8tC1NXh14i2liCht'
+twitter_consumer_secret = 'IzD68KY1D07Pgag6uQXxqnzHvoJyNy4my03dYP1YdMXQX4qAKi'
+twitter_access_token = '133288486-LZATl8ZJVy36z0fIfHNmHNppUz7AYZAQ7t7YzscF'
+twitter_access_secret = 'Js5tZqtOHye6PjJKLinfgKIuCcYsnvWHoLXPgk9r8Yn2N'
+
+
+
+pi_url = 'https://gateway.watsonplatform.net/personality-insights/api'
+pi_username = 'a2224ae4-365c-4bd8-95dc-9118f6851ed6'
+pi_password = 'XqfZ7pa7CDOy'
